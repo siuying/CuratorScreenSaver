94e3e524ccb667f20889cc55ea4fdcffd90dea35
Francis Chong <francis@ignition.hk>
2014-02-13 22:08:50 +0800

    Initial Commit

---
+++ CuratorScreenSaver/CuratorScreenSaverView.h
@@ -0,0 +1,13 @@
+//
+//  CuratorScreenSaverView.h
+//  CuratorScreenSaver
+//
+//  Created by Francis Chong on 2/13/14.
+//  Copyright (c) 2014 Ignition Soft. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface CuratorScreenSaverView : ScreenSaverView
+
+@end
---
+++ CuratorScreenSaver/CuratorScreenSaverView.m
@@ -0,0 +1,52 @@
+//
+//  CuratorScreenSaverView.m
+//  CuratorScreenSaver
+//
+//  Created by Francis Chong on 2/13/14.
+//  Copyright (c) 2014 Ignition Soft. All rights reserved.
+//
+
+#import "CuratorScreenSaverView.h"
+
+@implementation CuratorScreenSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
